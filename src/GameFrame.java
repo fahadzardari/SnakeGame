a11607d5efa6697d3397e679e921292d5ae60a7c
fahadzardari <fahadzardari111@gmail.com>
2022-07-09 23:26:53 +0500

    first commit

---
+++ src/GameFrame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("sdjkfsdjdk");
+    }
+}
